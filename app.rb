976500fa6c796bd9bc354e316d1312d36317c238
Adolfo Nava <adolfonava1990@gmail.com>
2024-04-30 17:10:41 +0000

    Dynamic rolling implementation

--- app.rb
+++ app.rb
@@ -52,3 +52,16 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:number_of_dice/:sidescount") do
+  @rolls = []
+
+  @num_of_rolls = params.fetch(:number_of_dice).to_i
+  @num_of_sides = params.fetch(:sidescount).to_i
+  
+  @num_of_rolls.times do
+    die = rand(1..@num_of_sides)
+    @rolls.push(die)
+  end
+  erb(:flexible)
+end
